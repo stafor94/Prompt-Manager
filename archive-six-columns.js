bd2051f20e8823a9e36f143c1f4beb967cff452b
stafor94 <stafor@naver.com>
2026-08-06 09:02:46 +0900

    chore: bump archive 6-column module to v1.0.7

--- archive-six-columns.js
+++ archive-six-columns.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0.6";
+const APP_VERSION = "1.0.7";
 const ARCHIVE_COLUMNS_KEY = "prompt-manager-archive-columns";
 const SIX_COLUMNS_KEY = "prompt-manager-archive-six-columns";
 
